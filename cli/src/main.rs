774fa86de4c2f5eb8405c90bb9208e2b39cddd13
Naqua Darazaki <n.darazaki@gmail.com>
2019-03-18 22:37:16 +0100

    Make CLI able to synchronize non-dirs paths

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,55 +9,48 @@ fn main() {
     let matches: clap::ArgMatches = clap_app!(("Keep Keeping CLI") =>
         (version: crate_version!())
         (author: crate_authors!())
-        (about: "Synchronizes directories together")
-        (@arg DIRS: +required ... "Directories to synchronize")
+        (about: "Synchronizes paths together")
+        (@arg PATHS: +required ... "Paths to synchronize")
     )
     .get_matches();
 
-    let dirs: Vec<_> = matches.values_of("DIRS").unwrap_or_default().collect();
+    let paths: Vec<_> = matches.values_of("PATHS").unwrap_or_default().collect();
 
-    match dirs.len().cmp(&2) {
+    match paths.len().cmp(&2) {
         Ordering::Less => {
-            eprintln!("You must precise at least 2 directories to synchronize.");
+            eprintln!("You must precise at least 2 paths to synchronize.");
             exit(1);
         }
         Ordering::Greater => {
-            eprintln!("Synchronizing more than 2 directories is not supported yet.");
+            eprintln!("Synchronizing more than 2 paths is not supported yet.");
             exit(1);
         }
-        Ordering::Equal => synchronize_or_exit(dirs[0], dirs[1]),
+        Ordering::Equal => synchronize_or_exit(paths[0], paths[1]),
     }
 }
 
 #[inline]
-fn synchronize_or_exit(dir1_str: &str, dir2_str: &str) {
-    let ok1 = check_dir(dir1_str);
-    let ok2 = check_dir(dir2_str);
+fn synchronize_or_exit(path1_str: &str, path2_str: &str) {
+    let path1 = Path::new(path1_str);
+    let path2 = Path::new(path2_str);
 
-    if ok1 && ok2 {
-        let dir1 = Path::new(dir1_str);
-        let dir2 = Path::new(dir2_str);
+    let path1_exists = path1.exists();
+    let path2_exists = path2.exists();
 
-        if let Err(err) = keep_keeping::synchronize(dir1, dir2) {
+    if path1_exists && path2_exists {
+        if let Err(err) = keep_keeping::synchronize(path1, path2) {
             eprintln!("{}", err);
             exit(1);
         }
     } else {
+        if !path1_exists {
+            eprintln!("Path does not exist: '{}'", path1_str);
+        }
+
+        if !path2_exists {
+            eprintln!("Path does not exist: '{}'", path2_str);
+        }
+
         exit(1);
-    }
-}
-
-#[inline]
-fn check_dir(dir_str: &str) -> bool {
-    let dir = Path::new(dir_str);
-
-    if !dir.exists() {
-        eprintln!("Directory does not exist: '{}'", dir_str);
-        false
-    } else if dir.is_dir() {
-        eprintln!("Path is not a directory: '{}'", dir_str);
-        false
-    } else {
-        true
     }
 }
